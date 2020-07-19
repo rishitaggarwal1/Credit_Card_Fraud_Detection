50fea96c269103998f348f8af13ea527c75d77e9
rishitaggarwal1 <rishitaggarwal1@gmail.com>
2020-07-19 23:54:00 +0530

    Data Reading

--- index.py
+++ index.py
@@ -1,15 +1,21 @@
-import sys
-import numpy
-import pandas
-import matplotlib
-import seaborn
+# importing libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import scipy
 import sklearn
+import sys
 
-print('Python: {}'.format(sys.version))
-print('Python: {}'.format(numpy.__version__))
-print('Python: {}'.format(pandas.__version__))
-print('Python: {}'.format(matplotlib.__version__))
-print('Python: {}'.format(seaborn.__version__))
-print('Python: {}'.format(scipy.__version__))
-print('Python: {}'.format(sklearn.__version__))
+
+# print('Python: {}'.format(sys.version))
+# print('Python: {}'.format(numpy.__version__))
+# print('Python: {}'.format(pandas.__version__))
+# print('Python: {}'.format(matplotlib.__version__))
+# print('Python: {}'.format(seaborn.__version__))
+# print('Python: {}'.format(scipy.__version__))
+# print('Python: {}'.format(sklearn.__version__))
+
+
+# load the dataset from csv file
+data = pd.read_csv('creditcard.csv')

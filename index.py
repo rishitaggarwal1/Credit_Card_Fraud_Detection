5c1e2e97c3908bcaf3ae9a55c834f0bfbe7921b6
rishitaggarwal1 <rishitaggarwal1@gmail.com>
2020-07-19 23:55:36 +0530

    Analyzing Data

--- index.py
+++ index.py
@@ -19,3 +19,17 @@ import sys
 
 # load the dataset from csv file
 data = pd.read_csv('creditcard.csv')
+
+# exploring the data
+print(data.columns)
+
+print(data.shape)
+
+print(data.describe())
+
+data = data.sample(frac=0.1, random_state=1)
+print(data.shape)
+
+# plot histogram
+data.hist(figsize=(20, 20))
+plt.show()

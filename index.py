65b5c0cb67660e9b4a5723bcb8f3363a93af6ff3
rishitaggarwal1 <rishitaggarwal1@gmail.com>
2020-07-19 23:51:55 +0530

    Import Libraries

---
+++ index.py
@@ -0,0 +1,15 @@
+import sys
+import numpy
+import pandas
+import matplotlib
+import seaborn
+import scipy
+import sklearn
+
+print('Python: {}'.format(sys.version))
+print('Python: {}'.format(numpy.__version__))
+print('Python: {}'.format(pandas.__version__))
+print('Python: {}'.format(matplotlib.__version__))
+print('Python: {}'.format(seaborn.__version__))
+print('Python: {}'.format(scipy.__version__))
+print('Python: {}'.format(sklearn.__version__))
